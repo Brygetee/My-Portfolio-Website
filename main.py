72b82b96e1d2d04f9197a0d7caf3a6b0aad4afd9
Brygetee Al-Shawkani <brygetee@gmail.com>
2026-09-16 12:01:05 -0400

    fixed tabs for orientation landscape

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
 
 
 # emojis: https://emojicombos.com/heart
-# TODO 1: Make tabs :before, :after the content element
-# TODO 2: make certificate links open on blanks
-#TODO 3: change text pink and dark pink variable colors
+
+# TODO 1: make certificate links open on blanks
+#Todo 2: correct contact page
+#Todo 3: add favicon to title
